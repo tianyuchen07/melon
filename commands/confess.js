4472b027ffd3911278f1ac5c39c100be0de5f222
Yuchen Tian <ych.tianyuchen@gmail.com>
2026-03-16 22:14:44 -0500

    Update confess.js

--- commands/confess.js
+++ commands/confess.js
@@ -2,6 +2,32 @@ module.exports = {
 	name: '',
 	description: '',
 	execute(message, args) {
-        
+        		if (message.channel.type !== 'dm'){return message.reply(`please DM Melon with your confession message using \`lon confess <confession>\``)}
+		if(!args[0]) return message.channel.send("Please send a message to confess.")
+		const confessMessage = args.slice(0).join(" ")
+		const mid = message.author.id
+		if (typeof confess[mid] == 'undefined') {
+			confess[mid] = [];
+			var newHex = randomHex()
+			var hexData = {
+				"hexCode": newHex
+			}
+			confess[mid].push(hexData);
+			save(confess, "confess.json")
+			const confessionEmbed = new Discord.MessageEmbed()
+				.setColor(confess[mid][confess[mid].length - 1].hexCode)
+				.setTitle("Confession")
+				.setDescription(confessMessage)
+		client.channels.cache.get('741881172908310559').send(confessionEmbed)
+		message.reply("Your confession has been sent!")
+		}
+		else{
+			const confessionEmbed = new Discord.MessageEmbed()
+				.setColor(confess[mid][confess[mid].length - 1].hexCode)
+				.setTitle("Confession")
+				.setDescription(confessMessage)
+		client.channels.cache.get('741881172908310559').send(confessionEmbed)
+		message.reply("Your confession has been sent!")
+		}
     }
 };
